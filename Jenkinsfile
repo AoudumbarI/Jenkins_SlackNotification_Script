86b90972c63fb21b9e83582cc4329c2fa8f96bb9
Aoudumbar Ingolle <aoudumbar.ingolle@techverito.com>
2022-08-25 11:52:27 +0530

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,38 @@
+@Library('sclak_library') _
+
+    try {
+        node {
+            
+        lib.start()
+
+        stage('Prepare code') {
+            echo 'do checkout stuff'
+        }
+
+        stage('Testing') {
+            echo 'Testing'
+            echo 'Testing - publish coverage results'
+        }
+
+        stage('Staging') {
+            echo 'Deploy Stage'
+        }
+
+        stage('Deploy') {
+            echo 'Deploy - Backend'
+            echo 'Deploy - Frontend'
+        }
+        stage('sh'){
+            sh './jenkins/scripts/deliver.sh'
+        }
+        
+        lib.success()
+        }
+    }
+
+   catch (err) {
+       
+     lib.fail()
+        
+    throw err
+  } 
---
+++ vars/lib.groovy
@@ -0,0 +1,13 @@
+ def start(){
+     slackSend teamDomain: "techverito", channel: "#gocd-build-notifications" , tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4", color: '#439FE0', 
+            message: " <${env.BUILD_URL}/console | ${env.JOB_NAME}> - #${env.BUILD_NUMBER} *STARTED* By ${currentBuild.getBuildCauses()[0].userId} "
+  }
+ def success(){
+   slackSend teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4", color: '#00FF00', 
+            message: "*SUCCESS* after ${currentBuild.durationString.minus(' and counting')} <${env.BUILD_URL}/console | ${env.JOB_NAME}> - #${env.BUILD_NUMBER} By ${currentBuild.getBuildCauses()[0].userId} "
+  }
+def fail() {
+   slackSend teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4", color: '#FF0000', 
+              message: "*FAILURE* after ${currentBuild.durationString.minus(' and counting')} <${env.BUILD_URL}/console | ${env.JOB_NAME}> - #${env.BUILD_NUMBER} By ${currentBuild.getBuildCauses()[0].userId} "
+}
+
