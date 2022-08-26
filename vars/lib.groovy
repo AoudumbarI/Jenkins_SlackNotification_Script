bbbc6633f22e50e0cd3bbfda38e705a491b74fc2
Aoudumbar Ingolle <aoudumbar.ingolle@techverito.com>
2022-08-26 18:54:42 +0530

    Finalone

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -5,38 +5,47 @@ def summery(){
 }
  
 def start(){
-     slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#439FE0', message:"*STARTED* By ${currentBuild.getBuildCauses()[0].userId} <${env.BUILD_URL}/console | ${env.JOB_NAME}> - #${env.BUILD_NUMBER}"
+     slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+     color: '#439FE0', message:"*STARTED* By ${currentBuild.getBuildCauses()[0].userId} <${env.BUILD_URL}/console | ${env.JOB_NAME}> - #${env.BUILD_NUMBER}"
 }
 
 def success(){
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#00FF00', message: "*SUCCESS* after ${summery()}"
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#00FF00', message: "*SUCCESS* after ${summery()}"
 }
 
 def fail() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#FF0000', message: "*FAILURE* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#FF0000', message: "*FAILURE* after ${summery()} "
 }
 
 def testing() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#00FF00', message: "*Testing-SUCCESS* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#00FF00', message: "*Testing-SUCCESS* after ${summery()} "
 }
 
 def staging() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#00FF00', message: "*Staging-SUCCESS* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#00FF00', message: "*Staging-SUCCESS* after ${summery()} "
 }
 
 def deploy() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#00FF00', message: "*Deploy-SUCCESS* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#00FF00', message: "*Deploy-SUCCESS* after ${summery()} "
 }
 
 def testingfail() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#FF0000', message: "*Testing-FAILURE* after ${summery()}"
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#FF0000', message: "*Testing-FAILURE* after ${summery()}"
 }
 
 def stagingfail() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#FF0000', message: "*Staging-FAILURE* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#FF0000', message: "*Staging-FAILURE* after ${summery()} "
 }
 
 def deployfail() {
-   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", color: '#FF0000', message: "*Deploy-FAILURE* after ${summery()} "
+   slackSend teamDomain: "${env.TEAMDOMAIN}", channel: "${env.CHANNEL}", tokenCredentialId: "${env.TOKENCREDENTIALID}", 
+   color: '#FF0000', message: "*Deploy-FAILURE* after ${summery()} "
 }
 
